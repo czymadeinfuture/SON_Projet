089943b123580b39aefd319277d51d6b39753257
czymadeinfuture <129965676+czymadeinfuture@users.noreply.github.com>
2024-02-20 16:17:01 +0100

    Update filtre_noise.ino

--- filtre_noise/filtre_noise.ino
+++ filtre_noise/filtre_noise.ino
@@ -8,6 +8,7 @@
 // WAV files for this and other Tutorials are here:
 // http://www.pjrc.com/teensy/td_libs_AudioDataFiles.html
 
+
 #include <Audio.h>
 #include <Wire.h>
 #include <SPI.h>
@@ -39,14 +40,35 @@ AudioControlSGTL5000     sgtl5000_1;
 //#define SDCARD_MOSI_PIN  11
 //#define SDCARD_SCK_PIN   13
 
+const char* songList[] = {"SDTEST1.WAV", "SDTEST2.WAV", "SDTEST3.WAV","SDTEST4.WAV"};
+int currentSongIndex = 0;  // 初始时设置为0
+int songListSize = sizeof(songList) / sizeof(songList[0]);
+
+void playNextSong() {
+  if (playSdWav1.isPlaying()) {
+    playSdWav1.stop();  // 如果当前有歌曲正在播放，先停止它
+  }
+
+  currentSongIndex++;  // 移动到下一首歌曲
+  if (currentSongIndex >= songListSize) {
+    currentSongIndex = 0;  // 如果已经是列表中的最后一首歌，回到列表的开始
+  }
+
+  playSdWav1.play(songList[currentSongIndex]);  // 播放下一首歌
+}
+
+void setGain(int gainLevel) {
+  float gain = gainLevel / 125.0;  
+  sgtl5000_1.volume(gain);  // 
+}
 
 void setup() {
   Serial.begin(9600);
   AudioMemory(8);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.5);
+  sgtl5000_1.volume(0.4);
   lowpass.setParamValue("freq",1000);
-  lowpass.setParamValue("NoiseLevel",0.005);
+  lowpass.setParamValue("NoiseLevel",0.05);
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
   if (!(SD.begin(SDCARD_CS_PIN))) {
@@ -59,10 +81,40 @@ void setup() {
 }
 
 void loop() {
-  if (playSdWav1.isPlaying() == false) {
-    Serial.println("Start playing");
-    playSdWav1.play("TEST.WAV");
-    delay(10); // wait for library to parse WAV info
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+
+    if (command == 'P' && playSdWav1.isPlaying() == false) {
+      //Serial.println("Start playing");
+      playSdWav1.play(songList[currentSongIndex]);
+      delay(5); // wait for library to parse WAV info
+    }
+    else if (command == 'S') {
+      if (playSdWav1.isPlaying()) {
+        playSdWav1.stop();
+        //Serial.println("Stopped");
+      }
+    }
+    else if (command == 'G') {
+      // wait for receiving the value of gain
+      while (Serial.available() < 1) {
+        delay(1);
+      }
+      int gain = Serial.read();
+      setGain(gain);
+      //Serial.println(gain);
+    }
+    else if (command == 'N') {
+      // wait for receiving the value of gain
+      while (Serial.available() < 1) {
+        delay(1);
+      }
+      int noiseread = Serial.read();
+      float noise = noiseread / 100.0;
+      lowpass.setParamValue("NoiseLevel", noise);
+    }
+    else if (command == 'X') {
+      playNextSong();
+    }
   }
-  // do nothing while playing...
 }
