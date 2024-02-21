f00738e3172b26e514646635c38841c9cb77675d
czymadeinfuture <129965676+czymadeinfuture@users.noreply.github.com>
2024-02-21 01:58:18 +0100

    Add files via upload

---
+++ projet/vinyl_simulator/thrumps.cpp
@@ -0,0 +1,76 @@
+#include "thrumps.h"
+#include <cmath>
+#include <cstdlib>
+#define MULT_16 32767
+#define SAMPLE_RATE_HZ 44100
+#define AUDIO_OUTPUTS 1
+#define decayFactor 0.08
+
+thrumps::thrumps() :
+AudioStream(AUDIO_OUTPUTS, new audio_block_t*[AUDIO_OUTPUTS])
+{
+    discon_time = 0.05;
+    tail_time = 0.5;
+    gap = 60.0/33.0;
+    is_thrump = false;
+    thrump_num = 1;
+}
+
+thrumps::~thrumps(){}
+
+void thrumps::Setplay(){
+    is_thrump = not is_thrump;
+}
+
+void thrumps::update(){
+    audio_block_t *block[AUDIO_OUTPUTS];
+    // Calculate the number of samples for each part of the thrump
+    float discon_Samples = round(discon_time * SAMPLE_RATE_HZ);
+    float tail_Samples = round(tail_time * SAMPLE_RATE_HZ);
+    if (is_thrump){
+        for (int channel = 0; channel < AUDIO_OUTPUTS; channel++) {
+            block[channel] = allocate();
+            if (block[channel]) { 
+                int index = 0;
+
+                // Generate the discontinuity
+                int i1 = 0;
+                while (i1 < discon_Samples) {
+                      if (index < AUDIO_BLOCK_SAMPLES) {
+                          block[channel]->data[index] = 1;             
+                          index++;
+                          i1++;
+                      }
+                      else {    
+                          transmit(block[channel], channel);
+                          release(block[channel]);
+                          block[channel] = allocate();
+                          index = 0;
+                      }
+                };
+                // Generate the tail
+                int i2 = 0;
+                while (i2 < tail_Samples) {
+                      if (index < AUDIO_BLOCK_SAMPLES) {
+                          float oscillation = sin(2 * M_PI * i2 * 1000 / SAMPLE_RATE_HZ);
+                          float decay = exp(-decayFactor * i2);
+                          float value = oscillation * decay;
+                          value = max(-1,min(1,value));    
+                          block[channel]->data[index] = value * MULT_16;
+                          index++;
+                          i2++;
+                      }
+                      else {
+                          transmit(block[channel], channel);
+                          release(block[channel]);
+                          block[channel] = allocate();
+                          index = 0;
+                      }
+                };
+                transmit(block[channel], channel);
+                release(block[channel]);
+                is_thrump = false;
+            }
+        }
+    }
+}
